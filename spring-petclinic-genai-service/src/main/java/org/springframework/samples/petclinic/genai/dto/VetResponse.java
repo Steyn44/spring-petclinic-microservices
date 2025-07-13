4bc0fe0237e328eae8c80af14d3e9b97c278200f
Steyn44 <103479337+Steyn44@users.noreply.github.com>
2025-07-13 10:49:07 +0530

    Update VetResponse.java

--- spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/dto/VetResponse.java
+++ spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/dto/VetResponse.java
@@ -3,4 +3,7 @@ package org.springframework.samples.petclinic.genai.dto;
 import java.util.List;
 
 public record VetResponse(List<String> vets) {
+    public List<String> vets() {
+        return vets;
+    }
 }
