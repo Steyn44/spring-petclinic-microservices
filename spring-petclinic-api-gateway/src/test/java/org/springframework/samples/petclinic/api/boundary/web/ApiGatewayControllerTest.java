8408792ccd3a7bf0d054f4d4a6971994ab9994ee
Steyn44 <103479337+Steyn44@users.noreply.github.com>
2025-07-15 12:02:55 +0530

    Update ApiGatewayControllerTest.java

--- spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/boundary/web/ApiGatewayControllerTest.java
+++ spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/boundary/web/ApiGatewayControllerTest.java
@@ -2,7 +2,7 @@ package org.springframework.samples.petclinic.api.boundary.web;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
